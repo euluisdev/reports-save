9a4533f32f2755465e97665a301aaa7ed7622e06
euluis-dev <fluisf00@gmail.com>
2025-05-08 20:38:19 -0300

    add feature: I developed a footer for the project and also created the css for it, and imported it into the main page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
+'use client';
+
+import styles from './page.module.css';
+
 export default function Footer() {
-    return (
-      <footer >
-        <p>&copy; 2025 - Meu Site</p>
-      </footer>
-    );
+  const currentYear = new Date().getFullYear();
+  
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.footerSection}>
+          <h3>Metrologia IEB-PE</h3>
+        </div>
+        
+        <div className={styles.footerBottom}>
+          <p>&copy; {currentYear} Sistema de Controle de Relatórios Dimensionais. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
   
