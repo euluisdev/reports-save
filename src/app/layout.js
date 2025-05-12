84118c9a58b0731ed0341b1b71241ccf62c670a4
euluis-dev <fluisf00@gmail.com>
2025-05-12 20:25:26 -0300

    change feat: I changed the linguage to the pt-br

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>
         <Navbar />
         <main>{children}</main>
